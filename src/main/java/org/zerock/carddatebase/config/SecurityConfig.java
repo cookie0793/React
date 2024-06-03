686db2a593d2d2a41047065e76db90a45336589f
tajo0793 <tajo0793@gmail.com>
2024-06-03 14:42:58 +0900

    잘못된 인증정보 요청시 오류 응답 처리

--- src/main/java/org/zerock/carddatebase/config/SecurityConfig.java
+++ src/main/java/org/zerock/carddatebase/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.zerock.carddatebase.component.AuthEntryPoint;
 import org.zerock.carddatebase.filter.AuthenticationFilter;
 
 /*
@@ -44,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
     private final AuthenticationFilter authenticationFilter;
+    private final AuthEntryPoint authEntryPoint;
 
     // 사용자인증을 위한 userDetailsService 설정/패스워드 암호화 알고리즘 설정
     // 암호를 DB에 저장하기 전에 BCrypt 암호화 처리
@@ -72,6 +74,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 // 다른 요청은 인증 과정을 거쳐야 접근할 수 있다.
                 .anyRequest().authenticated().and()
+                // 잘못된 인증정보 요청 시, 오류 응답 처리를 authEntryPoint가 한다.
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint).and()
                 // /login을 제외한 나머지 모든 요청은 필터를 통과해야 정상 응답을 받을 수 있다.
                 .addFilterBefore(authenticationFilter,
                         UsernamePasswordAuthenticationToken.class);
